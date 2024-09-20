00b44ef5a83f6cabdad1687cc18b61332ba4d55e
kbg2206 <jkushagra22@gmail.com>
2024-09-21 00:19:47 +0530

    Lec 2

---
+++ 01ViteReact/README.md
@@ -0,0 +1,6 @@
+steps to setup the project
+
+commands:
+1) npm create vite@latest
+select framework as react
+select language as javascript
---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+ 
+
+  return (
+   <div>
+     <h1>Welcome to React App</h1>
+   </div>
+  )
+}
+
+export default App;
