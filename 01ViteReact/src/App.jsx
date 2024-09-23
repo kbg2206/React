dce910b1fdf9503efa3786f88ad0026eae4f0a1e
kbg2206 <jkushagra22@gmail.com>
2024-09-24 01:17:25 +0530

    lec 4

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -3,11 +3,11 @@ import Chai from "./Chai.jsx";
 
 
 function App() {
- 
 
+  const username = "kushagra jain"
   return (
    <div>
-     <h1>Welcome to React App</h1>
+     <h1>Welcome to React App - {username}</h1>
      <Chai/>
    </div>
   )
---
+++ customReact/CustomReact.js
@@ -0,0 +1,29 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+
+
+const customRender = (reactElement,container) =>{
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute('href',reactElement.props.href);
+    // domElement.setAttribute('target',reactElement.props.target);
+    for (const prop in reactElement.props)
+    {
+        if(prop === "children") continue;
+        domElement.setAttribute(prop,reactElement.props[prop]);
+    }
+
+    container.appendChild(domElement);
+};
+const reactElement = {
+    type : 'a',
+    props : {
+        href:"http://google.com",
+        target:"_blank"
+    },
+    children : "click me to visit google"
+}
+
+const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer)
