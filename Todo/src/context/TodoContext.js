1050a37d195d0223b8cdf866b867166dd84fe3e9
kbg2206 <jkushagra22@gmail.com>
2024-10-06 01:07:41 +0530

    lec 14

---
+++ Todo/src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import { createContext , useContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id : 1,
+            todo : "Todo Msg",
+            completed : false,
+        }
+    ], 
+    addTodo: (todo)=>{},
+    removeTodo: (id)=>{},
+    toggleTodo: (id)=>{},
+    updateTodo: (id,todo)=>{},
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export function useTodo(){
+    return useContext(TodoContext);
+}
