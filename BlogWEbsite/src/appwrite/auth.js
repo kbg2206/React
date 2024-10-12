934c0fb1a8d3a120541da288f1a9cc06fab73beb
kbg2206 <jkushagra22@gmail.com>
2024-10-12 15:20:33 +0530

    lec 19

---
+++ BlogWEbsite/src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../conf/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client.setEndpoint(config.appwriteUrl);
+        this.client.setProject(config.appwriteProjectId);
+
+        this.account = new Account(client);
+    }
+
+    async createAccount({name,email,password}) {
+        try{
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
+            if(userAccount){
+                return this.login({email,password});
+            }
+            else{
+                throw new Error("Failed to create account");
+            }
+        }
+        catch(error){
+            throw error
+        }
+    }
+
+
+    async login({email, password}){
+        try{
+            const userSession = await this.account.createEmailPasswordSession(email, password);
+            return userSession;
+        }
+        catch(error){
+            throw error
+        }
+    }
+
+    async logout(){
+        try{
+            await this.account.deleteSessions();
+        }
+        catch(error){
+            throw error
+        }
+    }
+
+    async getUserProfile(){
+        try{
+            const user = await this.account.get();
+            return user;
+        }
+        catch(error){
+            throw error
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
